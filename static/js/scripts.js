fe19d06ee4a13e495d562109d0de4c68bcbd9a08
Serge Masyn <smasyn@users.noreply.github.com>
2024-12-05 18:16:49 +0100

    Add files via upload

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -4,3 +4,24 @@ const nav = document.querySelector('nav');
 hamburger.addEventListener('click', () => {
     nav.classList.toggle('active');
 });
+
+document.getElementById('contactform').addEventListener('submit', function(event) {
+    event.preventDefault();  // Prevent the form from submitting normally
+
+    const data = {
+        post_type      : "CONTACT",
+        conversation_id: document.getElementById('email').value,
+        message        : document.getElementById('message').value,
+    };
+
+    fetch('http://44.232.149.56', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),  // Convert form data to JSON
+    })
+    .then(response => response.json())
+    .then(responseData => console.log(responseData))
+    .catch(error => console.error('Error:', error));
+  });
